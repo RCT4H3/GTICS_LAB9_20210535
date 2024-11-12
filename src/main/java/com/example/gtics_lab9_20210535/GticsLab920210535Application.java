5e58ca5d6f2fb662ca4a1d5f62a69502f52d925b
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-11-11 19:30:36 -0500

    Nuevo proyecto

---
+++ src/main/java/com/example/gtics_lab9_20210535/GticsLab920210535Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20210535;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920210535Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab920210535Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtics_lab9_20210535/GticsLab920210535ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20210535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab920210535ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
